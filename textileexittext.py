63b94a79b748014e6894d1c0acdd88697d3efe7d
Qfwfq <notkrd@gmail.com>
2017-12-07 19:32:44 -0800

    Mostly corpus stuff

--- textileexittext.py
+++ textileexittext.py
@@ -13,12 +13,12 @@ IMAGE_WIDTH=750
 IMAGE_HEIGHT=1000
 WHITE_BG=255
 
-HORIZONTAL_CHARACTER_LIMIT = 18
+HORIZONTAL_CHARACTER_LIMIT = 17
 NEWLINE_TOLERANCE = 8
 LEFT_MARGIN = 20
 VERTICAL_SPACE = 200
 TOP_MARGIN = 20
-FONT_SIZE = 90
+FONT_SIZE = 96
 
 RALEWAY_EB = "fonts/Raleway-ExtraBold.ttf"
 JULIUS_REGULAR = "fonts/JuliusSansOne-Regular.ttf"
@@ -26,7 +26,8 @@ RALEWAY_MED = "fonts/Raleway-Medium.ttf"
 GEO_REG = "fonts/Geo-Regular.ttf"
 ROBOTO_MED = "fonts/Roboto-Medium.ttf"
 SPECTRAL_SB = "fonts/SpectralSC-SemiBold.ttf"
-FONT_LOCATION = GEO_REG
+TEKO_SB = "fonts/Teko-SemiBold.ttf"
+FONT_LOCATION = TEKO_SB
 
 THREADOF_FILE = "corpus/threadof_metaphors.txt"
 THEWEAVE_FILE = "corpus/theweave_metaphors.txt"
@@ -37,7 +38,11 @@ METAPHOR_FILES = [THREADOF_FILE, THEWEAVE_FILE, EMBROIDER_FILE, PINSANDNEEDLES_F
 
 THEWEAVE_TEMPLATES = "corpus/theweave_templates.txt"
 THREADOF_TEMPLATES = "corpus/threadof_templates.txt"
-TEMPLATE_FILES = {"weave": THEWEAVE_TEMPLATES, "thread": THREADOF_TEMPLATES}
+MISC_TEMPLATES = "corpus/assorted_templates.txt"
+EMBROIDER_TEMPLATES = "corpus/embroidery_templates.txt"
+TEXTILE_TEMPLATES = "corpus/textile_templates.txt"
+TEMPLATE_FILES = {"weave": THEWEAVE_TEMPLATES, "thread": THREADOF_TEMPLATES, "embroider": EMBROIDER_TEMPLATES, "textile": TEXTILE_TEMPLATES,
+                  "MISC": MISC_TEMPLATES}
 
 IN_PHRASES = "corpus/in.txt"
 WOULD_PHRASES = "corpus/would.txt"
@@ -46,8 +51,11 @@ PREPOSITION_PHRASES = "corpus/preposition.txt"
 NOUN_PHRASES = "corpus/noun_phrase.txt"
 VERB_PHRASES = "corpus/intransitive_verbs.txt"
 SENTENCES = "corpus/available_sentences.txt"
+ADJECTIVES = "corpus/available_sentences.txt"
+LOCATIONS = "corpus/locations.txt"
 PHRASE_FILES = {"IN": IN_PHRASES, "WOULD": WOULD_PHRASES, "IS": IS_PHRASES, 
                 "PREPOSITION_PHRASE": PREPOSITION_PHRASES, "NOUN_PHRASE": NOUN_PHRASES, "VERB_PHRASE": VERB_PHRASES,
+                "ADJECTIVE": ADJECTIVES, "LOCATION": LOCATIONS,
                 "SENTENCE": SENTENCES}
 
 OUTPUT_IMAGE_FILE = "exittext.bmp"
@@ -56,6 +64,15 @@ WORD_RE = re.compile("\w*")
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 fnt = ImageFont.truetype(os.path.join(__location__, FONT_LOCATION), FONT_SIZE)
+
+all_possible_lines = []
+for a_collection in METAPHOR_FILES:
+    file_reader = open(os.path.join(__location__, a_collection),"r")
+    all_possible_lines += file_reader.readlines()
+    file_reader.close()
+
+def pick_similar_line(line_in):
+    pass
 
 def txt_lineate(txt_in, character_limit=HORIZONTAL_CHARACTER_LIMIT, tolerance=NEWLINE_TOLERANCE):
     """ Inserts newlines into a string, to fit the image. 
@@ -98,9 +115,10 @@ def pick_pattern(trigger_str=""):
     
 def get_rand_response(from_file):
     """ Simply picks a random line from a given file. """
-    metaphors_str = open(os.path.join(__location__, from_file),"r")
-    possible_lines = metaphors_str.readlines()
+    file_reader = open(os.path.join(__location__, from_file),"r")
+    possible_lines = file_reader.readlines()
     txt_response = str(random.choice(possible_lines)).rstrip(string.punctuation+string.whitespace)
+    file_reader.close()
     return txt_response
     
 def get_vars(a_template):
@@ -112,7 +130,7 @@ def make_image(some_txt, output_file=OUTPUT_IMAGE_FILE):
     """Saves an image with given text. """
     stitch_pattern = Image.new("1", (IMAGE_WIDTH,IMAGE_HEIGHT), WHITE_BG)
     d = ImageDraw.Draw(stitch_pattern)
-    d.multiline_text((LEFT_MARGIN,TOP_MARGIN), some_txt, font=fnt)
+    d.multiline_text((LEFT_MARGIN,TOP_MARGIN), txt_lineate(some_txt.upper()), font=fnt)
     stitch_pattern.show()
     stitch_pattern.save(os.path.join(__location__, output_file))
 
@@ -120,5 +138,5 @@ def make_image(some_txt, output_file=OUTPUT_IMAGE_FILE):
 def stitch_image(text_in=""):
     """ Takes a string text_in as a query and outputs a file to output_file with the appropriate response """
     the_template = get_rand_response(pick_pattern(text_in))
-    the_text = txt_lineate(fill_template(the_template))
+    the_text = fill_template(the_template)
     make_image(the_text)
